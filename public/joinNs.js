3ee8c933bd1485158621b179672bf6dfb0668120
Derrick Mei <derrick.mei.168@gmail.com>
2020-05-19 02:23:39 -0500

    removed port

--- public/joinNs.js
+++ public/joinNs.js
@@ -10,7 +10,7 @@ function joinNs(endpoint) {
       .removeEventListener("submit", formSubmission);
   }
 
-  nsSocket = io(`https://slack-clone-derrick.herokuapp.com/${endpoint}`);
+  nsSocket = io(`https://slack-clone-derrick.herokuapp.com${endpoint}`);
   nsSocket.on("nsRoomLoad", (nsRooms) => {
     let roomList = document.querySelector(".room-list");
     roomList.innerHTML = "";
